5827a0ce5ee9cc43d42bbdd7bfa9b5317e3be04f
wowufoundme <shubhkirti.work@gmail.com>
2017-12-28 23:29:49 +0530

    Add Google Analytics

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-  $('.header__container').css('display','none');
-  $('.header__container').fadeIn(2000);
+    $('.header__container').css('display','none');
+    $('.header__container').fadeIn(2000);
+    $('.hamburger-box').click(function(){
+        $('.hamburger').toggleClass('is-active');
+        $('.sidebar').toggleClass('is-open');
+    });
 });
